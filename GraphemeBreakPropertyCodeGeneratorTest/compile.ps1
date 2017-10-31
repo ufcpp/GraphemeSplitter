d8ec77ea42c3d9c8a5d45b656830bcc7aca7a81c
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-10-31 23:10:27 +0900

    csc /o+

--- GraphemeBreakPropertyCodeGeneratorTest/compile.ps1
+++ GraphemeBreakPropertyCodeGeneratorTest/compile.ps1
@@ -1,13 +1,17 @@
 ﻿$csc = (Get-VSSetupInstance)[0].InstallationPath + '\MSBuild\15.0\Bin\Roslyn\csc.exe'
 
 # LinearIf: linear-search if
-& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.if.cs /out:bin\LinearIf.dll /target:library 
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.if.cs /out:bin\Debug\LinearIf.dll /target:library /o-
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.if.cs /out:bin\Release\LinearIf.dll /target:library /o+
 
 # Switch: switch (case x:)
-& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.switch.cs /out:bin\Switch.dll /target:library 
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.switch.cs /out:bin\Debug\Switch.dll /target:library /o-
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.switch.cs /out:bin\Release\Switch.dll /target:library /o+
 
 # SwitchWhen: switch (case uint x when min <= x && x <= max)
-& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.switchwhen.cs /out:bin\SwitchWhen.dll /target:library 
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.switchwhen.cs /out:bin\Debug\SwitchWhen.dll /target:library /o-
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.switchwhen.cs /out:bin\Release\SwitchWhen.dll /target:library /o+
 
 # BinarySearchIf: binary-search if
-& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.binif.cs /out:bin\BinarySearchIf.dll /target:library 
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.binif.cs /out:bin\Debug\BinarySearchIf.dll /target:library /o-
+& $csc ..\GraphemeBreakPropertyCodeGenerator\GraphemeBreakProperty.cs .\Benchmark.binif.cs /out:bin\Release\BinarySearchIf.dll /target:library /o+
